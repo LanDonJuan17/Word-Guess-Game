ae2e1419869cff07a3e90011bf19f254af6287e2
Landon Evans <evans.landon@gmail.com>
2019-06-20 07:20:48 -0500

    really frustrated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,68 @@
+(function () {
+    var chooseWord = function () {
+        var words = [
+            "galaxy",
+            "asteroid",
+            "stars",
+            "planet",
+            "sun",
+            "moon",
+            "universe",
+            "supernova",
+            "blackhole",
+            "comet"];
+
+        var wordChoice = words[Math.floor(Math.random() * words.length)];
+        document.getElementById("chosenWord").innerHTML =
+            wordChoice;
 
 
-var words = [
-    "galaxy",
-    "asteroid",
-    "stars",
-    "planet",
-    "sun",
-    "moon",
-    "universe",
-    "supernova",
-    "blackhole",
-    "comet"
-];
-var compWord = words[Math.floor(Math.random() * words.length)];
+        var wordSpaces = wordChoice.length;
+        for (var i = 0; i < wordlength; i++) {
+            wordChoice = [wordChoice.slice(0, i * 2 + 1), ' ', wordChoice.slice(i * 2 + 1)].join('');
+        }
+        var letters = "_";
+        var wordLength = wordChoice.length;
 
-var answer = [];
-for (var i = 0; i < compWord.length; i++) {
-    answer.push("_");
-}
+        for (i = 0; i < wordLength; i++) {
+            var x = wordChoice.charAt(i);
+        }
 
+        if (x === "" || x === "/'") {
+            blanks += x;
+        }
+        else {
+            blanks += "_";
+        }
+    }
+    document.getElementById("blanks").innerHTML = blanks;
+});
+
+document.onkeypress = function (keyPressed) {
+    var keyPressed = keyPressed || window.event,
+        charCode = keyPressed.keyCode || keyPressed.which,
+        userGuess = String.fromCharCode(charCode);
+    result = function () {
+        wordHolder = document.getElementById('hold');
+        correct = document.createElement('ul');
+        for (var i = 0; i < word.length; i++) {
+            correct.setAttribute('id', 'my-word');
+            guess = document.createElement('li');
+            guess.setAttribute('class', 'guess');
+            if (word[i] === "-") {
+                guess.innerHTML = "-";
+                space = 1;
+            } else {
+                guess.innerHTML = "_";
+            }
+
+            geusses.push(guess);
+            wordHolder.appendChild(correct);
+            correct.appendChild(guess);
+        }
+    }
+    var userGuess = keyPressed
+    document.getElementById("userGuess").innerHTML += userGuess;
+    document.getElementById("")
+
+};
